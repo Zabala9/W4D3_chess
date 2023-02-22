b75cad77216e8f85e192c0f59ac8298b37a85699
Steven Zabala <zabalasteven9@gmail.com>
2023-02-22 12:03:19 -0800

    Adding cursor and display files

--- board.rb
+++ board.rb
@@ -5,6 +5,7 @@ require_relative "knight.rb"
 require_relative "pawn.rb"
 require_relative "queen.rb"
 require_relative "bishop.rb"
+require_relative "null_piece.rb"
 
 class Board
     def initialize
